dc80848d212c109fae29e125f08ec26d18a6ff1d
augum <augumakuma@gmail.com>
2020-08-28 17:05:01 +0200

    second

--- src/app/services/commandemag.service.ts
+++ src/app/services/commandemag.service.ts
@@ -3,7 +3,6 @@ import { Injectable, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
 import { Lcommande } from '../model/lcommande';
 import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule,Validators }from '@angular/forms';
 import { ClientService } from './client.service';
@@ -22,7 +21,6 @@ export class CommandemagService implements OnInit {
   llivr;
   client;
   
- 
   constructor(private http:HttpClient,private toastr: ToastrService,private clientService:ClientService,
     private llservice:LcommandemagService) { }
     ngOnInit() {
